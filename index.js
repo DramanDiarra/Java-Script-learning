dcb1bd61d4ffce6a07d70ac2316f04ba28840e15
DDobrowolski <damian.dobrowolski99@gmail.com>
2023-03-05 02:07:07 +0100

    Added suggestions

--- index.js
+++ index.js
@@ -2,30 +2,15 @@ import { Builder, By, until } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
 import chromedriver from "chromedriver";
 import { Options } from "selenium-webdriver/chrome.js";
+import { JSDOM } from "jsdom";
+import fs from "fs";
 
-const scrapingSelectors = [
-  {
-    selector1:
-      "#kandydaci > section > div > ol > li:nth-child(1) > a > div > h3",
-    selector2:
-      "#kandydaci > section > div > ol > li:nth-child(1) > a > div > div.details-votes.single > div.details-votes-result.positive > b",
-    selector3: "#kandydaci > section > div > p > span > span",
-  },
-  // {
-  //   selector1:
-  //     "#kandydaci > section > div > ol > li:nth-child(2) > a > div > h3",
-  //   selector2:
-  //     "#kandydaci > section > div > ol > li:nth-child(2) > a > div > div.details-votes.single > div.details-votes-result.positive > b",
-  //   selector3: "#kandydaci > section > div > p > span > span",
-  // },
-  // {
-  //   selector1:
-  //     "#kandydaci > section > div > ol > li:nth-child(3) > a > div > h3",
-  //   selector2:
-  //     "#kandydaci > section > div > ol > li:nth-child(3) > a > div > div.details-votes.single > div.details-votes-result.positive > b",
-  //   selector3: "#kandydaci > section > div > p > span > span",
-  // },
-];
+const scrapingSelectors = {
+  selector1: "#kandydaci > section > div > ol > li:nth-child(1) > a > div > h3",
+  selector2:
+    "#kandydaci > section > div > ol > li:nth-child(1) > a > div > div.details-votes.single > div.details-votes-result.positive > b",
+  selector3: "#kandydaci > section > div > p > span > span",
+};
 
 const urls = [
   {
@@ -98,10 +83,31 @@ const urls = [
   //   url: "https://dzienniklodzki.pl/p/kandydaci/ona-i-on%2C1015735/?groupId=141265",
   // },
 ];
-let scrapingData;
+
+// Przekazywac z funkcji do funkcji, a nie definiowac na zewnatrz
+async function WebScrapingLocalTest(driver, lp) {
+  const { selector1, selector2, selector3 } = scrapingSelectors;
+
+  try {
+    await driver.wait(until.elementLocated(By.css(selector1)), 15000);
+    await driver.wait(until.elementLocated(By.css(selector2)), 15000);
+    await driver.wait(until.elementLocated(By.css(selector3)), 15000);
+
+    const couple = await driver.findElement(By.css(selector1));
+    const votes = await driver.findElement(By.css(selector2));
+    const area = await driver.findElement(By.css(selector3));
+    const coupleName = await couple.getText();
+    const votesValue = await votes.getText();
+    const areaName = await area.getText();
+
+    return { lp, coupleName, votesValue, areaName };
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 async function generateArray() {
-  scrapingData = [];
-  let lp = 1;
+  const scrapingData = [];
 
   for (let i = 0; i < urls.length; i++) {
     const options = new Options();
@@ -110,9 +116,6 @@ async function generateArray() {
       .forBrowser("chrome")
       .setChromeOptions(options)
       .build();
-    // const options = webdriver.ChromeOptions();
-    // options.add_experimental_option('excludeSwitches', ['enable-logging']);
-    // driver = webdriver.Chrome(options=options);
 
     let url = urls[i].url;
     await driver.get(url);
@@ -120,43 +123,18 @@ async function generateArray() {
     await driver.wait(until.elementLocated(By.css(privacySelector)), 15000);
     const acceptPrivacy = driver.findElement(By.css(privacySelector));
     acceptPrivacy.click();
-
-    async function WebScrapingLocalTest(selector1, selector2, selector3) {
-      try {
-        await driver.wait(until.elementLocated(By.css(selector1)), 15000);
-        await driver.wait(until.elementLocated(By.css(selector2)), 15000);
-        await driver.wait(until.elementLocated(By.css(selector3)), 15000);
-
-        const couple = await driver.findElement(By.css(selector1));
-        const votes = await driver.findElement(By.css(selector2));
-        const area = await driver.findElement(By.css(selector3));
-        const coupleName = await couple.getText();
-        const votesValue = await votes.getText();
-        const areaName = await area.getText();
-
-        scrapingData.push({ lp, coupleName, votesValue, areaName });
-        lp = lp + 1;
-      } catch (error) {
-        throw new Error(error);
-      }
-    }
-
-    await Promise.all(
-      scrapingSelectors.map(async ({ selector1, selector2, selector3 }) => {
-        await WebScrapingLocalTest(selector1, selector2, selector3);
-      })
-    );
+    scrapingData.push(await WebScrapingLocalTest(driver, i));
     await driver.close();
-    console.log(scrapingData);
   }
-  sortTableByVotes();
-  renderTable();
+  return scrapingData;
+  // sortTableByVotes(scrapingData);
+  // renderTable(scrapingData);
 }
 
-function renderTable() {
-  clearTable();
+function renderTable(scrapingData) {
+  const { document } = new JSDOM(fs.readFileSync("Ona_i_On.html")).window;
   const tableBody = document.querySelector("#Ona_i_On > tbody");
-  for (var i = 0; i < scrapingData.length; i++) {
+  for (let i = 0; i < scrapingData.length; i++) {
     const current = scrapingData[i];
     const tableRow = document.createElement("tr");
     tableRow.innerHTML = `<td>${current.lp}</td><td>${current.coupleName}</td><td>${current.votesValue}</td><td>${current.areaName}</td>`;
@@ -164,24 +142,21 @@ function renderTable() {
       tableRow.classList.add("us");
     tableBody.appendChild(tableRow);
   }
+
+  fs.writeFileSync("output.html", document.documentElement.outerHTML);
 }
 
-function clearTable() {
-  const tableBody = document.querySelector("#Ona_i_On > tbody");
-  tableBody.innerHTML = "";
-}
 
-function sortTableByVotes() {
-  scrapingData.sort((a, b) => b.votesValue - a.votesValue);
+function sortTableByVotes(scrapingData) {
+  const sorted = [...scrapingData].sort((a, b) => b.votesValue - a.votesValue);
   for (let i = 0; i < scrapingData.length; i++) {
     scrapingData[i].lp = i + 1;
   }
-  console.log(scrapingData);
+  return sorted;
 }
 
-generateArray();
+setInterval(async () => renderTable(sortTableByVotes(await generateArray())), 30000);
 
-// const interval = setInterval(() => {
-//   generateArray();
-//   sortTableByVotes();
-// }, 60000);
+// const scrapingData = generateArray();
+// const sorted = sortTableByVotes(scrapingData);
+// renderTable(sorted);
